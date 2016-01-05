d8bdb02ed4e1be78dd1661ad18c3cd9a114bfde4
Joel Edwards <joeledwards@gmail.com>
2016-01-04 22:45:04 -0700

    Cleaned up color defintions.

--- led.js
+++ led.js
@@ -2,9 +2,13 @@ const _ = require('lodash');
 const Q = require('q');
 const pi = require('./pi');
 
-const red = { number: 11, label: "red" };
-const green = { number: 13, label: "green" };
-const blue = { number: 15, label: "blue" };
+const red = { pins: [11], label: "red" };
+const green = { pins: [13], label: "green" };
+const blue = { pins: [15], label: "blue" };
+const yellow = { pins: [11, 13], label: "yellow" };
+const cyan = { pins: [13, 15], label: "cyan" };
+const magenta = { pins: [11, 15], label: "magenta" };
+const white = { pins: [11, 13, 15], label: "white" };
 
 const duration = 500;
 
@@ -13,22 +17,22 @@ function die(message) {
   pi.shutdown().then(() => process.exit(1));
 }
 
-function pulse(pins, duration) {
-  console.log(`Pin(s) ${_(pins).map((pin) => pin.number).value()}`);
-  return Q.allSettled(_(pins).map((pin) => pi.pulse(pin.number, duration)));
+function pulse(color, duration) {
+  console.log(`Pulsing ${color.label}; pins ${color.pins}`);
+  return Q.all(_(color.pins).map((pin) => pi.pulse(pin, duration)).value());
 }
 
 var stop = false;
 
 function cycle(count) {
   if (count > 0 && !stop) {
-    pulse([red], duration)
-    .then(() => pulse([green], duration))
-    .then(() => pulse([blue], duration))
-    .then(() => pulse([red, green], duration))
-    .then(() => pulse([green, blue], duration))
-    .then(() => pulse([blue, red], duration))
-    .then(() => pulse([red, green, blue], duration))
+    pulse(red, duration)
+    .then(() => pulse(green, duration))
+    .then(() => pulse(blue, duration))
+    .then(() => pulse(yellow, duration))
+    .then(() => pulse(cyan, duration))
+    .then(() => pulse(magenta, duration))
+    .then(() => pulse(white, duration))
     .then(() => cycle(count - 1))
     .catch((error) => {
       die(`Error controlling pin ${pin.number} for output: ${error}\n${error.stack}`);
